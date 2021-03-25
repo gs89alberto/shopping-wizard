c222b45dde736e1168f2f42ff84f3995ce35f1cb
Ethan <ctabbx@gmail.com>
2021-03-25 16:32:53 +0100

    Finish page

--- js/functions.js
+++ js/functions.js
@@ -58,6 +58,7 @@ function setBuyButtonCondition() {
     document.getElementById('conditions').addEventListener('change', function(event) {   
         document.getElementById('buyNowBtn').disabled = !event.target.checked;
     });
+    changePage();
 }
 
 
@@ -96,3 +97,110 @@ function loadComponent (file) {
         .catch (reject);
     });
   }
+
+//Principio Recojer Datos-----------------------------------------------------------------------------------------------------------------
+
+  //Start Page Vars
+let size;
+let colores;
+let imageShirt = {
+    "white":"img/whiteFront.jpg",
+    "black":"img/blackFront.jpg",
+    "blue":"img/blueFront.jpg",
+    "red":"img/redFront.jpg",
+    "green":"img/greenFront.jpg",
+}
+
+//Profile Page Vars
+let username;
+let email;
+let pasword;
+
+//Adress Page Vars
+let firstName;
+let lastName;
+let birthday;
+let adress1;
+let adress2;
+let postalCode;
+let country;
+let prefix;
+let telephone;
+let regularAdressConfirm = false;
+//Shipping Type Vars
+let price1;
+let option1;
+let productPrice = document.getElementById("productprice").getAttribute("value"); 
+
+
+//Start Page Functions
+function shirtSize(sizes){
+    size = sizes;
+}
+
+function shirtColor(colorS){
+    colores = colorS;
+    if(colores === "white"){
+        productPrice = 100;
+    }else if(colores === "black"){
+        productPrice = 120;
+    }else if(colores === "red"){
+        productPrice = 130;
+    }else if(colores === "blue"){
+        productPrice = 130;
+    }else if(colores === "green"){
+        productPrice = 130;
+    }
+}
+
+//Profile Page Functions
+function profileData(){
+    username = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    pasword = document.getElementById("pwd").value;
+}
+
+//Adress Page Functions
+function adressData(){
+    firstName = document.getElementById("firstName").value;
+    lastName = document.getElementById("lastName").value;
+    birthday = document.getElementById("birthday").value;
+    adress1 = document.getElementById("adress1").value;
+    adress2 = document.getElementById("adress2").value;
+    postalCode = document.getElementById("postalCode").value;
+    country = document.getElementById("country").value; 
+    prefix = document.getElementById("prefix").value;
+    telephone = document.getElementById("telephone").value;
+    let regularAdress = document.getElementById("checkItem");
+        if(regularAdress.checked){
+            regularAdressConfirm = true;
+        }
+    };
+
+//Shipping Type Functions
+function vasonovas(precio){
+    if(precio == "opt1"){
+        price1 = 0;
+        option1 = "72h";
+    }else if(precio == "opt2"){
+        price1 = 4.99;
+        option1 = "48h";
+    }else if(precio == "opt3"){
+        price1 = 9.99;
+        option1 = "24h";
+    }     
+}
+
+
+
+//Finsh Page Functions
+function changePage(){
+    document.getElementById("sizeSelected").innerHTML = size;
+    document.getElementById("colorSelected").style.backgroundColor=colores;
+    document.getElementById("delDateText").innerHTML = "In "+ option1;
+    document.getElementById("shippingPrice").innerHTML = price1 + "€";
+    document.getElementById("productprice").innerHTML = productPrice + "€";
+    document.getElementById("finishprice").innerHTML = productPrice + price1 + "€";
+    document.getElementById("purchaseImg").style.backgroundImage= "url("+imageShirt[colores]+")";
+}
+//Final Recojer Datos ------------------------------------------------------------------------------------------------------------------------------
