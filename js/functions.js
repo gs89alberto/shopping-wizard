2842b9e5497595307bc450abdad0c43b1ecd7dff
Alberto <gs89alberto@gmail.com>
2021-03-25 10:57:15 +0100

    templates fixed

--- js/functions.js
+++ js/functions.js
@@ -22,6 +22,9 @@ function setEventSubmitButton(elementId) {
     }
     if (elementId === 'shipping_NextBtn') setGiftCheckboxEvents();
     if (elementId === 'buyNowBtn') setBuyButtonCondition();
+    // if (elementId !== 'addToCartBtn' || elementId !=== 'buyNowBtn') {
+    //     showTimer();
+    // }
 }
 
 function nextStep(event) {
@@ -63,3 +66,33 @@ const popUpDiv = document.querySelector('#popUpDiv');
 function hide() {
     popUpDiv.classList.toggle('hidden');
 };
+
+function loadComponent (file) {
+    return new Promise ((resolve, reject) => {
+      fetch (file)
+        .then (result => {
+          return result.text ();
+        })
+        .then (txt => {
+          const parser           = new DOMParser ();
+          const fragment         = parser.parseFromString (txt, 'text/html');
+          const originalTemplate = fragment.getElementsByTagName ('TEMPLATE')[ 0 ];
+          let template;
+          if (originalTemplate) {
+            template           = document.createElement ('template');
+            template.innerHTML = originalTemplate.innerHTML;
+            template.id        = originalTemplate.id;
+            document.body.appendChild (template);
+          }
+          const originalScript = fragment.getElementsByTagName ('SCRIPT')[ 0 ];
+          let script;
+          if (originalScript) {
+            script           = document.createElement ('script');
+            script.innerHTML = originalScript.innerHTML;
+            document.body.appendChild (script);
+          }
+          resolve ({template, script});
+        })
+        .catch (reject);
+    });
+  }
